e041d888db8fba4aed42b81b5bb320fd17c6c7af
Jayanth Royal <gjayanthroyal1@gmail.com>
2026-03-17 19:00:49 +0530

    Added backend, frontend and docker setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is ruuning on 🚀 Port", PORT);
+});
