91c35aab44255beb7e2059e1146c7ec44192f5cb
Jayanth Royal <gjayanthroyal1@gmail.com>
2026-03-27 18:44:36 +0530

    corrected path for middleware

--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middlewares/auth.middleware");
-const upload = require("../middlewares/upload.middleware");
+const protect = require("../middleware/auth.middleware");
+const upload = require("../middleware/upload.middleware");
 const { uploadFile } = require("../controllers/upload.controller");
 
 router.post("/", protect, upload.single("file"), uploadFile);
